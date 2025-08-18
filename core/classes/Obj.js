05bc5a85e23c902ca0efcb6e5c7ca421a8f4a268
Jakub <jakubhanula0@gmail.com>
2025-08-18 15:42:56 +0200

    1.3.0.1

--- core/classes/Obj.js
+++ core/classes/Obj.js
@@ -1,14 +1,13 @@
 class Obj {
-  constructor({ transform, sprite, script }) {
+  constructor({ transform, sprite, script, start }) {
     this.transform = transform
     if (sprite) this.sprite = sprite
 
-    if (script) {
+    if (script)
       for (const key in script) {
-        if (script.hasOwnProperty(key)) {
-          this[key] = script[key]
-        }
+        if (script.hasOwnProperty(key)) this[key] = script[key]
       }
-    }
+
+    if (start) start()
   }
 }
